38cd22285600ca056a585384c83f1b66150ade17
Mantvydas Bražėnas <mantvydasbr7@gmail.com>
2021-10-23 14:56:47 +0300

    Created spinner and app layout

--- app/src/main/java/com/example/mipt_2/MainActivity.java
+++ app/src/main/java/com/example/mipt_2/MainActivity.java
@@ -4,16 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spOption;
+    EditText etSentenceInput;
+    TextView tvAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
-        Log.i("RoutineDealingWithExist", "onCreate =>");
+        this.spOption = findViewById(R.id.spOptions);
+        this.etSentenceInput = findViewById(R.id.etSentenceInput);
+        this.tvAnswer = findViewById(R.id.tvAnswer);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.counter_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.spOption.setAdapter(adapter);
     }
 }
