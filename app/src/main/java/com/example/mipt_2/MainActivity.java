d7ef162f7e0192585332981f897ef99701d5a86c
Mantvydas Bražėnas <mantvydasbr7@gmail.com>
2021-10-23 14:19:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mipt_2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.mipt_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+        Log.i("RoutineDealingWithExist", "onCreate =>");
+    }
+}
